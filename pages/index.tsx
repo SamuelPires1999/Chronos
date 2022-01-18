16b11bb0f51229e1c5917ef0a41316f30a0b495a
SamuelPires1999 <samuel.ferpires@gmail.com>
2022-01-18 19:39:58 -0300

    Changing project name at package.json

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import type { NextPage } from 'next'
-import Button from '../components/Button'
 
 const Home: NextPage = () => {
   return (
     <div className='w-screen h-screen flex items-center justify-center'>
-      <Button label='Button Label big as fuck'/>
+      something
     </div>
   )
 }
